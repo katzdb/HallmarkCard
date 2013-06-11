93d7f5fe17fb8ebe260fe8ab79a3c8e568b6b45e
Dani <danikatz11@gmail.com>
2013-06-11 00:38:49 -0400

    Create GetWell.java

---
+++ GetWell.java
@@ -0,0 +1,9 @@
+public class GetWell extends Card{
+public void makeCard() {
+  	genInfo();
+		System.out.println("Get Well Soon "+recipient);
+		System.out.println("Sincerely,");
+		System.out.println(from);
+		
+}
+}
