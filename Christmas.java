15f62de6c3109770f4b0517c766fd58b7bafb8d4
Dani <danikatz11@gmail.com>
2013-06-11 01:37:52 -0300

    Create Christmas.java

---
+++ Christmas.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+
+public class Christmas extends Card{
+public void makeCard() {
+  	genInfo();
+		System.out.println("Merry Christmas to "+ recipient);
+		System.out.println("from "+from);
+}
+}
